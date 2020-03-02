e66d4dd628e6a91c22312eaba84651d5593409d1
jungbum <jungbumwoo@naver.com>
2020-03-02 22:23:14 +0900

    0302

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,12 +3,13 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    requestSecret: async (_, args) => {
+    requestSecret: async (_, args, { request }) => {
       const { email } = args;
       const loginSecret = generateSecret();
       console.log(email);
       console.log(loginSecret);
       try {
+        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
