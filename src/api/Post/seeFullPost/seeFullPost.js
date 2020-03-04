68e93b52e625b658ffb887158bf48567d0e94d93
Jungbumwoo <jungbumwoo@Naver.com>
2020-03-04 10:18:00 +0900

    0304

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,10 +1,9 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGMENT, FULL_POST_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
     seeFullPost: async (_, args) => {
-      return prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
+      return prisma.post({ id });
     }
   }
 };
