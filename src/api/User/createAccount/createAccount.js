ee341377e188ee7a4bf67e27d31a1ab5d3cd4c72
Jungbumwoo <jungbumwoo@Naver.com>
2020-04-05 20:57:37 +0900

    0405

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,7 +3,11 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation : {
         createAccount: async(_, args) => {
-            const { username, email, firstname= "", lastname= "", bio = ""} = args;
+            const { username, email, firstName= "", lastName= "", bio = ""} = args;
+            const exists = await prisma.$exists.user({ username });
+            if (exists) {
+                throw Error("This username is already taken");
+            }
                 await prisma.createUser({
                     username,
                     email,
@@ -11,7 +15,7 @@ export default {
                     lastName,
                     bio
                 });
-                return ture;
+                return true;
         }
     }
 }
